95272fd8e73368b94047982bd93b87e639114574
André Soares <andre.soares@gdecorp.local>
2026-07-06 11:12:02 -0300

    Remove build artifacts from tracking, update .gitignore

--- .open-next/assets/_next/static/chunks/app/layout-2889de2e5337f4de.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{4109:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,9195,23))},9195:()=>{}},e=>{e.O(0,[450,441,964,358],()=>e(e.s=4109)),_N_E=e.O()}]);
